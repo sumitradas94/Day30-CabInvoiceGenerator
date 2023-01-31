f27c3e8939ccc4acb65a03345ae40ff10ebb6924
sumitradas94 <sumitradas94@gmail.com>
2023-02-01 00:00:54 +0530

    Cab Invoice Generator Problem

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace CabInvoiceGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Cab Invoice Generator.");
+
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine(fare);
+        }
+    }
+}
